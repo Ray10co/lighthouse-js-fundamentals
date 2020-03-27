623c53df04a153e207440ebc31a318f29c90a26b
Ray10co <raisa.ignatenco@gmail.com>
2020-03-27 02:43:33 +0000

    adding hello.js to github

---
+++ hello.js
@@ -0,0 +1,20 @@
+// hello world 
+/*
+
+const sayHello = function () {
+  console.log("hello, world");
+}
+sayHello();
+
+*/
+
+// hello name
+
+const sayHello = function(name) {
+  console.log("Hello", name);
+}
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
+
+
