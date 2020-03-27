18470124291b231d825e7c72cc335286337c8c92
Ray10co <raisa.ignatenco@gmail.com>
2020-03-27 03:14:21 +0000

    added odd & updated even

--- even.js
+++ even.js
@@ -13,10 +13,9 @@ console.log(elevenIsEven);
 
 // simplified code
 
-const isEVen = function (num) {
+const isEven  = function (num) {
   return num % 2 === 0;
 }
 
 console.log(isEven(10));
 console.log(isEven(11));
-
