61bade2747b8dbd34740f615534f2cb220035a27
Ray10co <raisa.ignatenco@gmail.com>
2020-03-27 03:01:53 +0000

    isEven exercise

---
+++ even.js
@@ -0,0 +1,22 @@
+/*
+
+const isEven = function (num) {
+  return num % 2 === 0;
+}
+const tenIsEven = isEven(10);
+const elevnIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
+
+*/
+
+// simplified code
+
+const isEVen = function (num) {
+  return num % 2 === 0;
+}
+
+console.log(isEven(10));
+console.log(isEven(11));
+
