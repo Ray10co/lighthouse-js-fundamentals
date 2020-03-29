32352ecad4785b65fbd116f88df0651e04c6be2f
Ray10co <raisa.ignatenco@gmail.com>
2020-03-29 03:13:26 +0000

    added banana bread ingredients array loops

---
+++ ingredients.js
@@ -0,0 +1,19 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", 
+"baking powder", "chocolate chips", "bananas"];
+
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i ++;
+};
+
+ 
+for(let b = 0; b < ingredients.length; b++) {
+  console.log(ingredients[b]);
+}
+
+for (let e = ingredients.length; e >= 0; e--) {
+  console.log(ingredients[e]);
+}
+
+
