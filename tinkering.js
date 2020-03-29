8ddc3ffdb73b1291b857079788a6ad30548b497c
Ray10co <raisa.ignatenco@gmail.com>
2020-03-29 04:21:29 +0000

    adding range loop bc compass wasn't accepting my answer

--- tinkering.js
+++ tinkering.js
@@ -234,4 +234,22 @@ function howManyHundreds(num) {
 }
 console.log(howManyHundreds(560));
 
-// 
+// range loop
+
+function range (start, end, step) {
+  let array = [];
+  for(let i = start; i <= end; i = i + step) {
+    array.push(i); 
+  }
+  if (start < end && end > start && step > 0) {
+    return array;
+  } else {
+    return [];
+  }
+}
+
+
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
+console.log(range(10, 5, 2));
